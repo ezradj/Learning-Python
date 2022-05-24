cbdc28cc93d8139c77fe8c223b68b1d03511794d
Ezra Johnson <ezradjohnson@gmail.com>
2022-05-24 00:12:21 -0700

    learning about 2D lists

--- main.py
+++ main.py
@@ -1,15 +1,7 @@
-# print("Why hello there!")
+drinks = ['coffee', 'soda', 'water']
+dinner = ['pizza', 'hamburger', 'hotdog']
+dessert = ['cake', 'ice cream']
 
-food = ["pizza", "tacos", "cheeseburgers", "pasta"]
+food = [drinks, dinner, dessert]
 
-food[0] = 'sushi'
-
-food.append("ice cream") #inserts an item at the beginning of the list
-food.remove("pasta") #removes the given item
-food.pop() #removes the last item in the list
-food.insert(0, 'cake') #inserts a given item at the given index
-food.sort() #sorts the list alphabetically
-food.clear() #removes everything from the list
-
-for x in food:
-  print(x)
+print(food[0][0]) #prints coffee
