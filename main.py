3b7f532f2f70200d6147d971bf76036c9a9208dd
Ezra Johnson <ezradjohnson@gmail.com>
2022-05-24 12:00:39 -0700

    learning about sets, lists, and tuples

--- main.py
+++ main.py
@@ -1,7 +1,35 @@
-drinks = ['coffee', 'soda', 'water']
-dinner = ['pizza', 'hamburger', 'hotdog']
-dessert = ['cake', 'ice cream']
+#drinks = ['coffee', 'soda', 'water']
+#dinner = ['pizza', 'hamburger', 'hotdog']
+#dessert = ['cake', 'ice cream']
 
-food = [drinks, dinner, dessert]
+#food = [drinks, dinner, dessert]
 
-print(food[0][0]) #prints coffee
+#print(food[0][0]) #prints coffee
+
+#student = ("Ezra", 16, "male")
+
+#print(student.index("male"))
+#print(student.count("Ezra"))
+
+#for x in student:
+  #print(x) #prints everything in the tuple, student.
+
+#if "Ezra" in student:
+  #print("Luke is here.") #prints Luke is here
+
+
+#utensils = {"fork", "spoon", "knife"} #creates a set, which is unorder, unindexed. No duplicate values.
+#dishes = {"bowl", "plate", "cup",}
+
+#utensils.add("napkin") #adds the given item into the set
+#utensils.remove("fork") #removes the given item from the set
+#utensils.clear() #removes everything from the set
+#utensils.update(dishes) #adds everything from dishes to utensils
+#dinner_table = utensils.union(dishes) # dinner_table is a set of both untensils and dishes
+
+#for x in dinner_table:
+  #print(x) #prints everything in utensils 
+
+#print(utensils.difference(dishes)) #prints what utensils has, that dishes doesn't have. (i.e. it is basically saying what utensils has, that dishes does not.)
+#print(utensils.intersection(dishes)) #prints what dishes and utensils has in common.#
+
